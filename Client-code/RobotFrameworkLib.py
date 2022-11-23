d6c5d04a76a3ba6dc8e00ed4a2a83da4e3c282ba
Holger Stenberg <holger.98@hotmail.com>
2022-11-23 08:40:55 +0100

    Updates to sprint5 planing
    Commit:
    a lot of Refactoring
    added "check" function for server replies
    added conversion functions for all peripherals relying on 16-bit values

--- Client-code/RobotFrameworkLib.py
+++ Client-code/RobotFrameworkLib.py
@@ -1,11 +1,9 @@
 
-
-#from HiL_client import status_instruction, turn_on_instruction, template_instruction
 import HiL_client
 import HiL_client_communication
-from HiL_config import BINARY_OBJECTS, TIME_UNITS
+from HiL_config import CONTROLLER_OBJECTS, CONTROLLER_GET_GROUPS, BINARY_OBJECTS, POT_OBJECTS, SHT20_OBJECTS, TIME_UNITS
 
-#The mathods are called from the robot framework file, and the instruction functions will handle the method call
+#The methods are called from the robot framework file, and the instruction functions will handle the method call
 class RobotFrameworkLib():
 
 	def run_status(self):
@@ -18,14 +16,30 @@ class RobotFrameworkLib():
 
 		#Check the length of the string, should be 1
 		if (len(string_list) != 1):
-			raise Exception("Input not correct length, expect 1")
+			raise Exception("Client: Input not correct length, expect 1")
 
 		#check if object is supported
 		elif string_list[0] not in BINARY_OBJECTS:
-			raise Exception("Object is not supported")
+			raise Exception("Client: Object is not supported")
 
 		else:
-			HiL_client.HiL_client_turn_on_instruction(string_list[0])
+			transaction_status = HiL_client.HiL_client_turn_on_instruction(string_list[0])
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				raise Exception("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				raise Exception("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				raise Exception("Server: Non-valid state request")
+			
+			else:
+				raise Exception("Server: Should not get here, investigate")
 
 
 	def turn_off (self,instruction):
@@ -34,15 +48,31 @@ class RobotFrameworkLib():
 
 		#Check the length of the string, should be 1
 		if (len(string_list) != 1):
-			raise Exception("Input not correct length, expect 1")
+			raise Exception("Client: Input not correct length, expect 1")
 
 		#check if object is supported
 		elif string_list[0] not in BINARY_OBJECTS:
-			raise Exception("Object is not supported")
+			raise Exception("Client: Object is not supported")
 
 		else:
-			HiL_client.HiL_client_turn_off_instruction(string_list[0])
-	
+			transaction_status = HiL_client.HiL_client_turn_off_instruction(string_list[0])
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				raise Exception("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				raise Exception("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				raise Exception("Server: Non-valid state request")
+			
+			else:
+				raise Exception("Server: Should not get here, investigate")
+
 
 	def push (self,instruction):
 
@@ -50,22 +80,39 @@ class RobotFrameworkLib():
 
 		#Check the length of the string, should be 4
 		if (len(string_list) != 4):
-			raise Exception("Input not correct length, expect 4")
+			raise Exception("Client: Input not correct length, expect 4")
 
 		#check if object is supported
 		elif string_list[0] not in BINARY_OBJECTS:
-			raise Exception("Object is not supported")
+			raise Exception("Client: Object is not supported")
 
 
 		elif string_list[1] != "for":
-			raise Exception("Missing word: for")
+			raise Exception("Client: Missing word: for")
 
 
 		elif string_list[3] not in TIME_UNITS:
-			raise Exception("Time unit is not supported")
+			raise Exception("Client: Time unit is not supported")
 
 		else:
-			HiL_client.HiL_client_push_instruction(string_list)
+			transaction_status = HiL_client.HiL_client_push_instruction(string_list)
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				print("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				print("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				print("Server: Non-valid requested state")
+			
+			else:
+				print("Server: Should not get here, investigate")
+
 	
 	def tune (self,instruction):
 
@@ -73,23 +120,108 @@ class RobotFrameworkLib():
 
 		#Check the length of the string, should be 3
 		if (len(string_list) != 3):
-			raise Exception("Input not correct length, expect 3")
+			raise Exception("Client: Input not correct length, expect 3")
 
 		#check if object is supported
-		elif string_list[0] != "POT":
-			raise Exception("Object is not supported, should be POT")
+		elif string_list[0] not in POT_OBJECTS:
+			raise Exception("Client: Object is not supported")
 	
 
 		elif string_list[1] != "to":
-			raise Exception("Missing word: to")
-
+			raise Exception("Client: Missing word: to")
 
 		else:
-			HiL_client.HiL_client_tune_instruction(string_list)
+			transaction_status = HiL_client.HiL_client_tune_instruction(string_list)
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				print("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				print("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				print("Server: Non-valid requested state")
+			
+			else:
+				print("Server: should not get here, investigate")
 	
 
+	def set_temperature (self,instruction):
 
-	def set_temp (self,instruction):
+		string_list = instruction.split(" ")
+
+		#Check the length of the string, should be 3
+		if (len(string_list) != 4):
+			raise Exception("Input not correct length, expect 3")
+		
+		#check if object is supported
+		elif string_list[1] not in SHT20_OBJECTS:
+			raise Exception("Object is not supported")
+
+
+		elif string_list[2] != "to":
+			raise Exception("Missing word: to")
+
+		else:
+			transaction_status = HiL_client.HiL_client_set_temperature_instruction(string_list)
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				print("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				print("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				print("Server: Non-valid requested state")
+			
+			else:
+				print("Server: Should not get here, investigate")
+
+
+	def set_humidity (self,instruction):
+
+		string_list = instruction.split(" ")
+
+		#Check the length of the string, should be 3
+		if (len(string_list) != 4):
+			raise Exception("Input not correct length, expect 3")
+		
+		#check if object is supported
+		elif string_list[1] not in SHT20_OBJECTS:
+			raise Exception("Object is not supported")
+		
+		elif string_list[2] != "to":
+			raise Exception("Missing word: to")
+		
+		else:
+			transaction_status = HiL_client.HiL_client_set_humidity_instruction(string_list)
+
+			#Server Error Checking
+			if (transaction_status == 0):
+				pass
+			
+			elif (transaction_status == 1):
+				print("Server: Non-specified error")
+
+			elif (transaction_status == 2):
+				print("Server: Object not supported")
+
+			elif (transaction_status == 3):
+				print("Server: Non-valid requested state")
+			
+			else:
+				print("Server: Should not get here, investigate")
+
+
+	def check_if (self, instruction): 
 
 		string_list = instruction.split(" ")
 
@@ -98,39 +230,21 @@ class RobotFrameworkLib():
 			raise Exception("Input not correct length, expect 3")
 		
 		#check if object is supported
-		elif string_list[0] != "TEMP_SENSOR":
-			raise Exception("Object is not supported, should be TEMP_SENSOR")
+		elif string_list[0] not in CONTROLLER_OBJECTS:
+			raise Exception("Object is not supported")
 
-	
-		elif string_list[1] != "to":
-			raise Exception("Missing word: to")
-
+		elif CONTROLLER_OBJECTS[string_list[0]].object_get_group not in CONTROLLER_GET_GROUPS:
+			raise Exception("Group is not supported")
 
 		else:
-			HiL_client.HiL_client_set_temp_instruction(string_list)
+			reply = HiL_client.HiL_client_check_if_instruction(string_list)
 
-	def set_humi (self,instruction):
+			if int(string_list[2]) != reply:
+				raise Exception("Wrong reply (expected: {}, given: {})".format(string_list[2],reply))
 
-		string_list = instruction.split(" ")
-
-		#Check the length of the string, should be 3
-		if (len(string_list) != 3):
-			raise Exception("Input not correct length, expect 3")
-		
-		#check if object is supported
-		elif string_list[0] != "HUMI_SENSOR":
-			raise Exception("Object is not supported, should be HUMI_SENSOR")
-		
-		elif string_list[1] != "to":
-			raise Exception("Missing word: to")
-		
-		else:
-			HiL_client.HiL_client_set_humi_instruction(string_list)
-
-	def template_keyword(self):
-		HiL_client.template_instruction()
 
 # The following codes are just for debuging this file
 obj = RobotFrameworkLib()
 if __name__=="__main__":
-	obj.set_humi("HUMI_SENSOR to 45.7")
+	
+	obj.set_humidity("of HUMI_SENSOR to 0")
