324f6daa97923a632042a673a0f86073f81ca4bd
Fredrik <fkar8@kth.se>
2022-12-14 16:50:24 +0100

    need to fix crc function, but otherwise it should work

--- Board-code/HiL_Board/Debug/Core/Src/HiL_gateway.d
+++
@@ -1,4 +0,0 @@
-Core/Src/HiL_gateway.o: ../Core/Src/HiL_gateway.c ../Core/Inc/setup.h \
- ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h
-../Core/Inc/setup.h:
-../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
